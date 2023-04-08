d928f3e281d63b8b57c3176e140ca355a3ab56c1
M.P.C PRIYAMANTHA <95483538+ChandimalPriyamantha@users.noreply.github.com>
2023-04-08 21:52:01 +0530

    changes

--- TowSum/src/Map.java
+++ TowSum/src/Map.java
@@ -1,30 +1,30 @@
-import java.util.HashMap;
-
-public class Map {
-
-    public static void main(String[] args) {
-        HashMap<String, Integer> empIds = new HashMap<>();
-
-        empIds.put("Chandimal",12345);
-        empIds.put("Tharuka",123234);
-        empIds.put("Sadeepa",121235);
-
-        System.out.println(empIds);
-        System.out.println(empIds.get("Chandimal"));
-        System.out.println(empIds.containsKey("Tharuka"));
-        System.out.println(empIds.containsValue(12345));
-
-        empIds.put("Chandimal", 8765543);
-        System.out.println(empIds);
-
-        empIds.putIfAbsent("Chandimal",2334);
-        System.out.println(empIds);
-
-        empIds.putIfAbsent("Kamal",2334);
-        System.out.println(empIds);
-
-        empIds.remove("Chandimal");
-        System.out.println(empIds);
-    }
-
-}
+//import java.util.HashMap;
+//
+//public class Map {
+//
+//    public static void main(String[] args) {
+//        HashMap<String, Integer> empIds = new HashMap<>();
+//
+//        empIds.put("Chandimal",12345);
+//        empIds.put("Tharuka",123234);
+//        empIds.put("Sadeepa",121235);
+//
+//        System.out.println(empIds);
+//        System.out.println(empIds.get("Chandimal"));
+//        System.out.println(empIds.containsKey("Tharuka"));
+//        System.out.println(empIds.containsValue(12345));
+//
+//        empIds.put("Chandimal", 8765543);
+//        System.out.println(empIds);
+//
+//        empIds.putIfAbsent("Chandimal",2334);
+//        System.out.println(empIds);
+//
+//        empIds.putIfAbsent("Kamal",2334);
+//        System.out.println(empIds);
+//
+//        empIds.remove("Chandimal");
+//        System.out.println(empIds);
+//    }
+//
+//}
--- TowSum/src/Solution.java
+++ TowSum/src/Solution.java
@@ -1,34 +1,28 @@
 import java.util.*;
 class Solution {
 
-//        public int[] towSum(int[] A, int target){
-//            int N = A.length;
-//            int []out = new int[2];
-//
-//            Map<Integer, Integer> map = new HashMap<>();
-//
-//            for(int i=0; i<N; i++){
-//                int b = target - A[i];
-//
-//                if(map.containsKey(b)){
-//                    out[0] = Math.min(i, map.get(b));
-//                    out[1] = Math.max(i, map.get(b));
-//                    return out;
-//                }else{
-//                    map.put(A[i], i);
-//                }
-//            }
-//
-//            return null;
-//        }
+        public int[] towSum(int[] A, int target){
+            int N = A.length;
+            int []out = new int[2];
+
+            Map<Integer, Integer> map = new HashMap<>();
+
+            for(int i=0; i<N; i++){
+                int b = target - A[i];
+
+                if(map.containsKey(b)){
+                    out[0] = Math.min(i, map.get(b));
+                    out[1] = Math.max(i, map.get(b));
+                    return out;
+                }else{
+                    map.put(A[i], i);
+                }
+            }
+
+            return null;
+        }
 
 }
 
-class Run{
-    public static void main(String[] args) {
 
-
-
-    }
-}
 
